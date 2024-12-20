12fc41661e76b1fc1f7e5ec3b7fc17e83c090c36
Harish <84895830+MrXhibit@users.noreply.github.com>
2024-12-20 11:08:08 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <>
+        <div class="container">
+          <p class="m-0 text-center text-white">
+            Copyright <a href="mailTo:harish.b2209@gmail.com">©www.HBShop.com</a>{" "}
+            2024
+          </p>
+        </div>
+      </>
+    </div>
+  );
+}
+
+export default Footer
